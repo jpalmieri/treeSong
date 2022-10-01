141844b3d8fd3a70e37cdc4c16a343f56f88bf10
Joe Palmieri <joe.palmieri@hingehealth.com>
2022-10-01 15:30:53 -0700

    allow multiple p5 canvases

--- roots.js
+++ roots.js
@@ -1,78 +1,82 @@
-const containerId = 'roots';
+const rootsSketch = p => {
+  const containerId = 'roots';
 
-let roots = [];
+  let roots = [];
 
-function setup() {
-  const { containerWidth, containerHeight} = getContainerDimensions();
-  const canvas = createCanvas(containerWidth, containerHeight);
-  canvas.parent(containerId);
-
-  background(255);
-
-  roots.push(new Root({ x: width / 2, y: 0 }));
-}
-
-class Root {
-  x;
-  y;
-
-  length = 200;
-
-  stepsTaken = 0;
-
-  direction;
-
-  constructor({ x, y, direction = {
-    x: 1,
-    y: 1
-  } }) {
-    this.x = x;
-    this.y = y;
-    this.direction = direction;
+   p.setup = function() {
+    const { containerWidth, containerHeight} = getContainerDimensions();
+    const canvas = p.createCanvas(containerWidth, containerHeight);
+    canvas.parent(containerId);
+  
+    p.background(255);
+  
+    roots.push(new Root({ x: p.width / 2, y: 0 }));
   }
-
-  step() {
-    if (this.stepsTaken == this.length / 2) {
-      console.log("new ROot")
-      const direction = { x: -this.direction.x, y: this.direction.y };
-      const newRoot = new Root({ direction, x: this.x, y: this.y });
-      roots.push(newRoot);
+  
+  class Root {
+    x;
+    y;
+  
+    length = 200;
+  
+    stepsTaken = 0;
+  
+    direction;
+  
+    constructor({ x, y, direction = {
+      x: 1,
+      y: 1
+    } }) {
+      this.x = x;
+      this.y = y;
+      this.direction = direction;
     }
-
-    if (this.stepsTaken < this.length) {
-      this.x += this.direction.x;
-      this.y += this.direction.y;
-      this.stepsTaken++;
+  
+    step() {
+      if (this.stepsTaken == this.length / 2) {
+        console.log("new ROot")
+        const direction = { x: -this.direction.x, y: this.direction.y };
+        const newRoot = new Root({ direction, x: this.x, y: this.y });
+        roots.push(newRoot);
+      }
+  
+      if (this.stepsTaken < this.length) {
+        this.x += this.direction.x;
+        this.y += this.direction.y;
+        this.stepsTaken++;
+      }
+  
+      
     }
-
-    
+  
+    display() {
+      p.stroke(0);
+      p.point(this.x,this.y);
+    }
   }
-
-  display() {
-    stroke(0);
-    point(this.x,this.y);
+  
+  p.draw = function() {
+    roots.forEach(root => {
+      root.step();
+      root.display();
+    })
   }
-}
-
-function draw() {
-  roots.forEach(root => {
-    root.step();
-    root.display();
-  })
-}
-
-// This Redraws the Canvas when resized
-windowResized = function () {
-  const { containerWidth, containerHeight} = getContainerDimensions();
-  resizeCanvas(containerWidth, containerHeight);
+  
+  // This Redraws the Canvas when resized
+  windowResized = function () {
+    const { containerWidth, containerHeight} = getContainerDimensions();
+    resizeCanvas(containerWidth, containerHeight);
+  };
+  
+  function getContainerDimensions() {
+    const container = document.getElementById(containerId);
+    const containerWidth = container.offsetWidth;
+    const containerHeight = container.offsetHeight;
+    return {
+      containerWidth,
+      containerHeight
+    }
+  }
 };
 
-function getContainerDimensions() {
-  const container = document.getElementById(containerId);
-  const containerWidth = container.offsetWidth;
-  const containerHeight = container.offsetHeight;
-  return {
-    containerWidth,
-    containerHeight
-  }
-}
+new p5(rootsSketch);
--- tree.js
+++ tree.js
@@ -1,186 +1,189 @@
-const plantBorder = 50;
-const nodeBorder = 0;
-const minBranchLength = 20;
-const maxBranchLength = 85;
-const minSizeMultiplier = .55;
-const treeId = 'tree';
+const treeSketch = p => {
+  const plantBorder = 50;
+  const nodeBorder = 0;
+  const minBranchLength = 20;
+  const maxBranchLength = 85;
+  const minSizeMultiplier = .55;
+  const treeId = 'tree';
 
-let plant;
-let plantX;
-let plantY;
+  let plant;
+  let plantX;
+  let plantY;
 
-function setup() {
-  // createCanvas(400, 400);
-  const { containerWidth, containerHeight} = getContainerDimensions();
-  const canvas = createCanvas(containerWidth, containerHeight);
-  canvas.parent(treeId);
+  p.setup = function() {
+    // createCanvas(400, 400);
+    const { containerWidth, containerHeight} = getContainerDimensions();
+    const canvas = p.createCanvas(containerWidth, containerHeight);
+    canvas.parent(treeId);
 
-  plantX = width / 2;
-  plantY = height * .9;
-  plant = new Node(null, 50, random(200, 255), TAU * 0.75);
-  strokeWeight(0);
-  circleColor = random(255);
+    plantX = p.width / 2;
+    plantY = p.height * .9;
+    plant = new Node(null, 50, p.random(200, 255), p.TAU * 0.75);
+    p.strokeWeight(0);
+    circleColor = p.random(255);
 
-}
-
-function draw() {
-  background(255,255,255);
-
-  plant.draw();
-  plant.maybeGrow();
-
-  noStroke();
-  fill(120, 0, 0);
-  quad(plantX - 25, plantY -140,
-       plantX + 25, plantY -140,
-       plantX + 25, plantY + 55,
-       plantX - 25, plantY + 55);
-
-   fill(0, 0, 0);
-    noStroke();
-    ellipse(200,290,20,30);
-}
-
-function mousePressed() {
-  const clickedNode = plant.getClickedNode(mouseX, mouseY);
-  if (clickedNode) {
-    clickedNode.prune();
-  }
-}
-
-class Node {
-
-  constructor(parent, parentSize, parentG, angle) {
-    this.parent = parent;
-    this.parentSize = parentSize;
-    this.size = random(parentSize * minSizeMultiplier, parentSize);
-    this.angle = angle;
-    this.branchLength = random(minBranchLength, maxBranchLength);
-    this.children = [];
-
-    this.x = parent ? this.getX(parent.x) : plantX;
-    this.y = parent ? this.getY(parent.y) : plantY;
-
-    this.g = parentG + random(-25, 25);
-    this.g = constrain(this.g, 0, 255);
   }
 
-  getX(parentX) {
-    return parentX + cos(this.angle) *
-      (this.parentSize / 2 + this.branchLength + this.size / 2);
+  p.draw = function() {
+    p.background(255,255,255);
+
+    plant.draw();
+    plant.maybeGrow();
+
+    p.noStroke();
+    p.fill(120, 0, 0);
+    p.quad(plantX - 25, plantY -140,
+        plantX + 25, plantY -140,
+        plantX + 25, plantY + 55,
+        plantX - 25, plantY + 55);
+
+    p.fill(0, 0, 0);
+    p.noStroke();
   }
 
-  getY(parentY) {
-    return parentY + sin(this.angle) *
-      (this.parentSize / 2 + this.branchLength + this.size / 2);
+  p.mousePressed = function() {
+    const clickedNode = plant.getClickedNode(p.mouseX, p.mouseY);
+    if (clickedNode) {
+      clickedNode.prune();
+    }
   }
 
-  grow() {
-    const childAngle = this.angle + random(-PI / 3, PI / 3);
-    const child = new Node(this, this.size, this.g, childAngle);
+  class Node {
 
-    if (child.size < 5) {
-      return false;
+    constructor(parent, parentSize, parentG, angle) {
+      this.parent = parent;
+      this.parentSize = parentSize;
+      this.size = p.random(parentSize * minSizeMultiplier, parentSize);
+      this.angle = angle;
+      this.branchLength = p.random(minBranchLength, maxBranchLength);
+      this.children = [];
+
+      this.x = parent ? this.getX(parent.x) : plantX;
+      this.y = parent ? this.getY(parent.y) : plantY;
+
+      this.g = parentG + p.random(-25, 25);
+      this.g = p.constrain(this.g, 0, 255);
     }
 
-    if(child.x < plantBorder || child.x > width - plantBorder){
-      return false;
+    getX(parentX) {
+      return parentX + p.cos(this.angle) *
+        (this.parentSize / 2 + this.branchLength + this.size / 2);
     }
 
-    if (child.y < plantBorder || child.y > plantY - 25) {
-      return false;
+    getY(parentY) {
+      return parentY + p.sin(this.angle) *
+        (this.parentSize / 2 + this.branchLength + this.size / 2);
     }
 
-    if (plant.intersects(this, child.x, child.y, child.size)) {
-      return false;
-    }
+    grow() {
+      const childAngle = this.angle + p.random(-p.PI / 3, p.PI / 3);
+      const child = new Node(this, this.size, this.g, childAngle);
 
-    this.children.push(child);
-    return true;
-  }
-
-  maybeGrow() {
-    const grew = this.grow();
-
-    if (!grew) {
-      const randomChild = random(this.children);
-      if (randomChild) {
-        randomChild.maybeGrow();
+      if (child.size < 5) {
+        return false;
       }
-    }
-  }
 
-  prune() {
-    // can't prune the first node
-    if (this == plant) {
-      return;
-    }
-    const index = this.parent.children.indexOf(this);
-    this.parent.children.splice(index, 1);
-  }
-
-  getClickedNode(clickedX, clickedY) {
-    if (dist(this.x, this.y, clickedX, clickedY) < this.size / 2) {
-      return this;
-    }
-
-    for (const child of this.children) {
-      const clickedChildNode = child.getClickedNode(clickedX, clickedY);
-      if (clickedChildNode) {
-        return clickedChildNode;
+      if(child.x < plantBorder || child.x > p.width - plantBorder){
+        return false;
       }
-    }
 
-    return null;
-  }
+      if (child.y < plantBorder || child.y > plantY - 25) {
+        return false;
+      }
 
-  intersects(parentNode, otherNodeX, otherNodeY, otherNodeSize) {
-    if (this != parentNode &&
-      dist(this.x, this.y, otherNodeX, otherNodeY) <
-           this.size / 2 + otherNodeSize / 2 + nodeBorder) {
+      if (plant.intersects(this, child.x, child.y, child.size)) {
+        return false;
+      }
+
+      this.children.push(child);
       return true;
     }
 
-    for (const child of this.children) {
-      if (child.intersects(parentNode, otherNodeX, otherNodeY, otherNodeSize)) {
-        return true;
+    maybeGrow() {
+      const grew = this.grow();
+
+      if (!grew) {
+        const randomChild = p.random(this.children);
+        if (randomChild) {
+          randomChild.maybeGrow();
+        }
       }
     }
 
-    return false;
-  }
-
-  draw() {
-
-    // branches
-    stroke(139, 69, 19);
-    strokeWeight(1);
-    for (const child of this.children) {
-      line(this.x, this.y, child.x, child.y);
+    prune() {
+      // can't prune the first node
+      if (this == plant) {
+        return;
+      }
+      const index = this.parent.children.indexOf(this);
+      this.parent.children.splice(index, 1);
     }
 
-    // circle
-      noStroke();
-    fill(circleColor);
-    circle(this.x, this.y, this.size);
+    getClickedNode(clickedX, clickedY) {
+      if (p.dist(this.x, this.y, clickedX, clickedY) < this.size / 2) {
+        return this;
+      }
 
- if (mouseIsPressed) {
-  circleColor = color(random (255), random(255), random(255));
-}
+      for (const child of this.children) {
+        const clickedChildNode = child.getClickedNode(clickedX, clickedY);
+        if (clickedChildNode) {
+          return clickedChildNode;
+        }
+      }
 
-    // children
-    for (const child of this.children) {
-      child.draw();
+      return null;
+    }
+
+    intersects(parentNode, otherNodeX, otherNodeY, otherNodeSize) {
+      if (this != parentNode &&
+        p.dist(this.x, this.y, otherNodeX, otherNodeY) <
+            this.size / 2 + otherNodeSize / 2 + nodeBorder) {
+        return true;
+      }
+
+      for (const child of this.children) {
+        if (child.intersects(parentNode, otherNodeX, otherNodeY, otherNodeSize)) {
+          return true;
+        }
+      }
+
+      return false;
+    }
+
+    draw() {
+
+      // branches
+      p.stroke(139, 69, 19);
+      p.strokeWeight(1);
+      for (const child of this.children) {
+        p.line(this.x, this.y, child.x, child.y);
+      }
+
+      // circle
+      p.noStroke();
+      p.fill(circleColor);
+      p.circle(this.x, this.y, this.size);
+
+      if (p.mouseIsPressed) {
+        circleColor = p.color(p.random (255), p.random(255), p.random(255));
+      }
+
+      // children
+      for (const child of this.children) {
+        child.draw();
+      }
+    }
+  }
+
+  function getContainerDimensions() {
+    const container = document.getElementById(treeId);
+    const containerWidth = container.offsetWidth;
+    const containerHeight = container.offsetHeight;
+    return {
+      containerWidth,
+      containerHeight
     }
   }
 }
 
-function getContainerDimensions() {
-  const container = document.getElementById(treeId);
-  const containerWidth = container.offsetWidth;
-  const containerHeight = container.offsetHeight;
-  return {
-    containerWidth,
-    containerHeight
-  }
-}
+new p5(treeSketch);
